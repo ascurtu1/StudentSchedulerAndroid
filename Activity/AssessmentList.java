44d7869bb62eccf34694816ff8e94a6986ac3d8f
Alexandra Scurtu <alexscurtu00@gmail.com>
2023-08-18 16:42:34 -0500

    new commit

---
+++ Activity/AssessmentList.java
@@ -0,0 +1,4 @@
+package com.example.studentscheduler.Activity;
+
+public class AssessmentList {
+}
--- Activity/MainActivity.java
+++ Activity/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.studentscheduler.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 import com.example.studentscheduler.R;
 
@@ -12,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    Button TermsBtn = findViewById(R.id.TermsBtn);
+    TermsBtn.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, TermList.class);
+            startActivity(intent);
+        }
+    });
+
+
+        Button CoursesBtn = findViewById(R.id.CoursesBtn);
+        CoursesBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CourseList.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button AssessmentsBtn = findViewById(R.id.AssessmentsBtn);
+        AssessmentsBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AssessmentList.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
